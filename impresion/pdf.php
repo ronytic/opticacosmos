f6a094ec415a3dde90d191d96ec67adde5a59ee9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-10-04 08:41:14 -0400

    Corregido Pdf

--- impresion/pdf.php
+++ impresion/pdf.php
@@ -23,7 +23,14 @@ include_once("fpdf_protection.php");
 			$this->SetSubject(utf8_decode("Sistema de Administración de Clinica por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
 			$this->SetCreator(utf8_decode("Sistema de Administración de Clinica por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
 			$this->SetProtection(array('print'));
-			if($this->CurOrientation=="P"){$this->ancho=176;}else{$this->ancho=246;}
+			if($this->CurOrientation=="P"){$this->ancho=$this->w-34;}else{$this->ancho=$this->w-40;}	
+			/*
+			if($this->CurOrientation=="P"){
+				$this->Line($this->w-16,46,$this->w-16,$this->h-15);
+			}else{
+				$this->Line($this->w-22,46,$this->w-22,$this->h-15);	
+			}*/
+			
 			$this->SetLeftMargin(18);
 			$this->SetAutoPageBreak(true,15);
 			global $title,$gestion,$titulo,$logo,$idioma;
@@ -154,5 +161,111 @@ include_once("fpdf_protection.php");
 				$this->Pie();	
 			}
 		}
+		///Tabla
+		var $widths;
+		var $aligns;
+		
+		function SetWidths($w)
+		{
+			//Set the array of column widths
+			$this->widths=$w;
+		}
+		
+		function SetAligns($a)
+		{
+			//Set the array of column alignments
+			$this->aligns=$a;
+		}
+		function Borde($borde=true){
+			$this->bordep=$borde;
+		}
+		function Row($data)
+		{
+			//Calculate the height of the row
+			$nb=0;
+			for($i=0;$i<count($data);$i++)
+				$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+			$h=5*$nb;
+			//Issue a page break first if needed
+			$this->CheckPageBreak($h);
+			//Draw the cells of the row
+			for($i=0;$i<count($data);$i++)
+			{
+				$w=$this->widths[$i];
+				$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+				//Save the current position
+				$x=$this->GetX();
+				$y=$this->GetY();
+				if(true){
+				//Draw the border
+				$this->Rect($x,$y,$w,$h);
+				}
+				//Print the text
+				$this->MultiCell($w,5,$data[$i],0,$a);
+				//Put the position to the right of the cell
+				$this->SetXY($x+$w,$y);
+			}
+			//Go to the next line
+			$this->Ln($h);
+		}
+		
+		function CheckPageBreak($h)
+		{
+			//If the height h would cause an overflow, add a new page immediately
+			if($this->GetY()+$h>$this->PageBreakTrigger)
+				$this->AddPage($this->CurOrientation);
+		}
+		
+		function NbLines($w,$txt)
+		{
+			//Computes the number of lines a MultiCell of width w will take
+			$cw=&$this->CurrentFont['cw'];
+			if($w==0)
+				$w=$this->w-$this->rMargin-$this->x;
+			$wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+			$s=str_replace("\r",'',$txt);
+			$nb=strlen($s);
+			if($nb>0 and $s[$nb-1]=="\n")
+				$nb--;
+			$sep=-1;
+			$i=0;
+			$j=0;
+			$l=0;
+			$nl=1;
+			while($i<$nb)
+			{
+				$c=$s[$i];
+				if($c=="\n")
+				{
+					$i++;
+					$sep=-1;
+					$j=$i;
+					$l=0;
+					$nl++;
+					continue;
+				}
+				if($c==' ')
+					$sep=$i;
+				$l+=$cw[$c];
+				if($l>$wmax)
+				{
+					if($sep==-1)
+					{
+						if($i==$j)
+							$i++;
+					}
+					else
+						$i=$sep+1;
+					$sep=-1;
+					$j=$i;
+					$l=0;
+					$nl++;
+				}
+				else
+					$i++;
+			}
+			return $nl;
+		}
+		/// Fin Tabla
 	}
 ?>
