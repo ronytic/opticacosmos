b65d62b1b174d19b452d85e03047a2fb4efb05a8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-11-14 17:09:53 -0400

    Corregido Inicio

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ $titulo="NPaginaPrincipal";
 <script language="javascript" type="text/javascript" src="js/core/plugins/highcharts.js"></script>
 <!--<script language="javascript" type="text/javascript" src="js/inicio.js"></script>-->
 <?php include_once("cabecera.php");?>
-
+<center>
+<img src="imagenes/inicio2.jpg" width="100%" class="img-polaroid">
+</center>
 <?php include_once("pie.php");?>
