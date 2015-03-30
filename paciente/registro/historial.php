ae2e69ff5240594caec8e72e2d4852936a1a712b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-03-30 06:09:49 -0400

    Cambio de Historial

--- paciente/registro/historial.php
+++ paciente/registro/historial.php
@@ -48,50 +48,41 @@ $(document).on("ready",function(){
                 <a onClick="javascript:history.back();" class="btn btn-xs"><?php echo $idioma['VolverSeleccionarPaciente']?></a>
             </div>
         </div>
-        
-        <div class="widget-header widget-header-flat"><h4><?php echo $idioma['NuevaCitaMedica']?></h4></div>
-        <div class="widget-body widget-main">
-        	<form action="tipoarea.php" method="post">
-            <?php campo("CodPaciente","hidden",$Cod)?>
-			<table class="table table-bordered">
-            	<thead>
-                	<tr>
-						<th><?php echo $idioma['Area'] ?></th>
-                        <th></th>
-                    </tr>
-                </thead>
-            	<tr>
-                	<td><?php campo("Area","select",$areas,"",1)?></td>
-                    <td><?php campo("","submit",$idioma['RegistrarNuevaCita'],"btn btn-success")?></td>
-                </tr>
-            </table>
-			</form>
-        </div>
         <div class="widget-header widget-header-flat"><h4><?php echo $idioma['CitasMedicas']?></h4></div>
         <div class="widget-body widget-main">
         	<div class="">
-                <form action="../areas/listado.php" method="post" class="formulario">
+                <form action="historialpaciente.php" method="post" class="formulario">
                 	<?php campo("CodPaciente","hidden",$Cod,"",0)?>
-                	<div class="col-sm-4">
-                        <div class="input-daterange input-group FechaRangoIdependiente">
-                            <label><?php echo $idioma['Desde']?></label>
-                            <input type="text" class="input-sm form-control" name="Desde" value="<?php echo fecha2Str(); ?>" />
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>
+                                    <label><?php echo $idioma['Desde']?></label>
+                                    <input type="date" class="input-sm form-control" name="Desde" value="<?php echo fecha2Str("",0,"-7 day"); ?>" />
+                                    </th>
+                                    <th>
+                                    <label><?php echo $idioma['Hasta']?></label>
+                                    <input type="date" class="input-sm form-control" name="Hasta" value="<?php echo fecha2Str("",0,"0day"); ?>"/>
+                                    </th>
+                                    <th>
+                                    <label><?php echo $idioma['NumeroBoleta']?></label>
+                                    <input type="text" class="input-sm form-control" name="NumeroBoleta" value=""/>        
+                                    </th>
+                                    <th>
+                                    <?php campo("","submit",$idioma['Buscar'],"btn btn-success")?>
+                                    </th>
+                                </tr>
+                            </thead>
+                        </table>
+                            
                             <span class="input-group-addon"></span>
-                            <label><?php echo $idioma['Hasta']?></label>
-                            <input type="text" class="input-sm form-control" name="Hasta" value="<?php echo fecha2Str(); ?>"/>
-                  			
-                        </div>
-                    </div>
+                            
+
                     <div class="col-sm-2">
-                    <label><?php echo $idioma['NumeroBoleta']?></label>
-                            <input type="text" class="input-sm form-control" name="NumeroBoleta" value=""/>        
+                    
                     </div>  
-                    <div class="col-sm-2">
-                    	<label ><?php echo $idioma['Area']?></label><br>
-                    	<?php campo("Area","select",$areas,"",0,0,"","","Optica")?>
-                    </div>
                     <div class="col-sm-4"><br>	
-                    	<?php campo("","submit",$idioma['Buscar'],"btn btn-success")?>
+                    	
                     </div>
                     <div class="clearfix"></div>
                     
