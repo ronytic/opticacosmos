d0fac13c1aa6e9bc241830626d29bd7713fe7f66
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-03-29 21:25:27 -0400

    Boleta

---
+++ venta/registro/boleta.php
@@ -0,0 +1,30 @@
+<?php
+include_once("../../login/check.php");
+$NoRevisar=1;
+$folder="../../";
+$Cod=$_GET['CodOptica'];
+if($Cod==""){
+	$Cod=$_GET['Cod'];	
+}
+$titulo="NVerBoletaOrdenTrabajo";
+$url="../../impresion/areas/ordentrabajo.php?Cod=".$Cod;
+include_once($folder."cabecerahtml.php");
+?>
+<script language="javascript">
+configuracion={todayBtn: "", endDate: "'0d'"};
+</script>
+<?php include_once($folder."cabecera.php");?>
+<div class="col-sm-12">
+	<div class="widget-box">
+    	<div class="widget-header widget-header-flat"><h4><?php echo $idioma['Reporte'] ?></h4></div>
+        <div class="widget-body">
+        	<div class="widget-main">
+            	<a href="<?php echo $url?>" class="btn btn-danger btn-xs" target="_blank"><?php echo "Abrir la Boleta en Otra Ventana"?></a>
+                
+                <hr>
+            	<iframe src="<?php echo $url?>" width="100%" height="500" frameborder="1"></iframe>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include_once($folder."pie.php");?>
