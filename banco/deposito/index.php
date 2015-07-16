d751fb20a799b56882802816f9179adb70a4aa3a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-16 11:58:57 -0400

    Deposito

--- banco/deposito/index.php
+++ banco/deposito/index.php
@@ -6,6 +6,10 @@ include_once("../../class/banco.php");
 $banco=new banco;
 $ban=todolista($banco->mostrarTodoRegistro("",1,"Nombre"),"CodBanco","Nombre,NumeroCuenta,TipoCuenta"," - ");
 
+include_once("../../class/depositario.php");
+$depositario=new depositario;
+$dep=todolista($depositario->mostrarTodoRegistro("",1,"Paterno,Materno,Nombres"),"CodDepositario","Paterno,Materno,Nombres"," ");
+
 include_once($folder."cabecerahtml.php");
 ?>
 <script language="javascript">
@@ -18,20 +22,23 @@ configuracion={todayBtn: false, endDate: "'0d'"};
     	<td class="der">Banco</td>
         <td colspan="2"><?php campo("CodBanco","select",$ban,"",1)?></td>
     </tr>
-	
+	<tr>
+    	<td class="der">Depositario</td>
+        <td colspan="2"><?php campo("CodDepositario","select",$dep,"",1)?></td>
+    </tr>
     <tr>
     	<td class="der">Número de Boleta</td>
         <td colspan="2"><?php campo("Nombres","text","","",1)?></td>
     </tr>
     <tr>
     	<td class="der">Glosa</td>
-        <td colspan="2"><?php campo("Glosa","textarea")?></td>
+        <td colspan="2"><?php campo("Glosa","textarea","",0,"","","",array("cols"=>40,"rows"=>5))?></td>
     </tr>
     
     <tr>
-        <td></td>
-        <td class="">Monto en Bolivianos: <br><?php campo("MontoBolivianos","text","","",0)?></td>
-    	<td class=""> Monto en Dolares:<br><?php campo("MontoDolares","text","","",0)?></td>
+        <td class="der">Monto Depositado</td>
+        <td class=""><?php campo("Monto","number","","",0)?></td>
+
 
     </tr>
     <tr>
---
+++ class/depositario.php
@@ -0,0 +1,8 @@
+<?php
+include_once("bd.php");
+class depositario extends bd{
+	var $tabla="depositario";
+
+	
+}
+?>
