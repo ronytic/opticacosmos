7c189c7b286ffa23abc63bf9545c01eace584c62
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 12:25:17 -0400

    Eliminar Depositario

--- banco/depositarios/eliminar.php
+++ banco/depositarios/eliminar.php
@@ -1,10 +1,10 @@
 <?php  
 include_once("../../login/check.php");
 if (!empty($_GET)) {
-	$nombre="depositario";
+	$nombre="bancodepositario";
 	include_once '../../class/'.$nombre.'.php';
 	${$nombre}=new $nombre;
 	$Cod=$_GET['Cod'];
-	${$nombre}->eliminarRegistro("CodEspecialidad=".$Cod);
+	${$nombre}->eliminarRegistro("CodBancoDepositario=".$Cod);
 }
 ?>
