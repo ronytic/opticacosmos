e8e12930a89b7339863ad4a4578531524e2cb5de
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 10:04:09 -0400

    Eliminar Deposito

--- banco/deposito/eliminar.php
+++ banco/deposito/eliminar.php
@@ -1,10 +1,10 @@
 <?php  
 include_once("../../login/check.php");
 if (!empty($_GET)) {
-	$nombre="medico";
+	$nombre="deposito";
 	include_once '../../class/'.$nombre.'.php';
 	${$nombre}=new $nombre;
 	$Cod=$_GET['Cod'];
-	${$nombre}->eliminarRegistro("CodMedico=".$Cod);
+	${$nombre}->eliminarRegistro("CodDeposito=".$Cod);
 }
 ?>
