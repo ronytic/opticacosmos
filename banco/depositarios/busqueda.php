824391932daeb4b01359b87cd0062e3cda76188c
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 12:24:14 -0400

    Listar Depositarios

--- banco/depositarios/busqueda.php
+++ banco/depositarios/busqueda.php
@@ -1,14 +1,14 @@
 <?php
 include_once("../../login/check.php");
-include_once("../../class/depositario.php");
-$depositario=new depositario;
+include_once("../../class/bancodepositario.php");
+$bancodepositario=new bancodepositario;
 extract($_POST);
 $Nombres=$Nombres!=""?"Nombres LIKE '$Nombres%'":"Nombres LIKE '%'";
 $Paterno=$Paterno!=""?"Paterno LIKE '$Paterno%'":"Paterno LIKE '%'";
 $Materno=$Materno!=""?"Materno LIKE '$Materno%'":"Materno LIKE '%'";
 
 $condicion="$Nombres and $Paterno and $Materno";
-$dep=$depositario->mostrarTodoRegistro($condicion,1,"Paterno,Materno,Nombres");
+$dep=$bancodepositario->mostrarTodoRegistro($condicion,1,"Paterno,Materno,Nombres");
 $titulo=array(	"Nombres"=>$idioma['Nombres'],
 				"Paterno"=>$idioma['ApellidoPaterno'],
                 "Materno"=>$idioma['ApellidoMaterno'],
