ff8ad493b64948cb9a9cba204307bc27e60da683
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 17:42:36 -0400

    Boleta

--- venta/registro/guardaroptica.php
+++ venta/registro/guardaroptica.php
@@ -222,7 +222,7 @@ configuracion={todayBtn: "", endDate: "'0d'"};
             	<a href="<?php echo $url?>" class="btn btn-success btn-xs" target="_blank"><?php echo "Ver Boleta en Otra Ventana"?></a>
                 <a href="<?php echo $folder?>factura/venta/?Cod=<?php echo $Cod?>" class="btn btn-danger">Facturar</a>
                 <hr>
-            	<iframe src="<?php echo $url?>" width="100%" height="700" frameborder="1"></iframe>
+            	<iframe src="<?php echo $url?>" width="100%" height="735" frameborder="1"></iframe>
 			</div>
 		</div>
 	</div>
