b53f60d15314cc47b91676f90f2a92f55160a619
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 13:35:44 -0400

    Banco Eliminar

---
+++ banco/retiro/eliminar.php
@@ -0,0 +1,10 @@
+<?php  
+include_once("../../login/check.php");
+if (!empty($_GET)) {
+	$nombre="bancoretiro";
+	include_once '../../class/'.$nombre.'.php';
+	${$nombre}=new $nombre;
+	$Cod=$_GET['Cod'];
+	${$nombre}->eliminarRegistro("CodBancoRetiro=".$Cod);
+}
+?>
