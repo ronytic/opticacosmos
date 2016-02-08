cb4131a202bdd63af93a120a94ff567c07f7b589
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-08 17:07:00 -0400

    Guardar Optica

--- venta/registro/guardaroptica.php
+++ venta/registro/guardaroptica.php
@@ -190,5 +190,41 @@ $Nuevo=0;
 $NoRevisar=1;
 
 $folder="../../";
-include_once("../../resultado.php");
+//include_once("../../resultado.php");
 ?>
+<?php
+//include_once("../../login/check.php");
+$NoRevisar=1;
+$folder="../../";
+$Cod=$opt['CodOptica'];
+if($Cod==""){
+	$Cod=$_GET['Cod'];	
+}
+$titulo="NVerBoletaOrdenTrabajo";
+$url="../../impresion/optica/ordentrabajo".$UbicacionReporte.".php?Cod=".$Cod;
+include_once($folder."cabecerahtml.php");
+?>
+<script language="javascript">
+configuracion={todayBtn: "", endDate: "'0d'"};
+</script>
+<?php include_once($folder."cabecera.php");?>
+<div class="col-sm-12">
+	<div class="widget-box">
+    	<div class="widget-header widget-header-flat"><h4><?php echo $idioma['Reporte'] ?></h4></div>
+        <div class="widget-body">
+        	<div class="widget-main">
+                <ul class="list-unstyled">
+                	<?php foreach($Mensajes as $m){?>
+                	<li><i class="icon-angle-right bigger-110"></i><?php echo $m ?></li>
+                    <?php }?>
+                </ul>
+                <hr class="separador">
+            	<a href="<?php echo $url?>" class="btn btn-success btn-xs" target="_blank"><?php echo "Ver Boleta en Otra Ventana"?></a>
+                <a href="<?php echo $folder?>factura/venta/?Cod=<?php echo $Cod?>" class="btn btn-danger">Facturar</a>
+                <hr>
+            	<iframe src="<?php echo $url?>" width="100%" height="700" frameborder="1"></iframe>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include_once($folder."pie.php");?>
