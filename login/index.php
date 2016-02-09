e764c45f9db0740d18ce8b5826f862db44436f3a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-09 13:27:22 -0400

    Index

--- login/index.php
+++ login/index.php
@@ -14,7 +14,8 @@ if($_COOKIE['Idioma']=="" || empty($_COOKIE['Idioma'])){
 }
 //$anuncioslogin=new anuncioslogin;
 $config=new config;
-$title=$config->mostrarConfig("Titulo",1);
+$TituloSistema=$config->mostrarConfig("TituloSistema",1);
+$Titulo=$config->mostrarConfig("Titulo",1);
 $sigla=$config->mostrarConfig("Sigla",1);
 $LogoInicio=$config->mostrarConfig("LogoInicio",1);
 $LogoIcono=$config->mostrarConfig("LogoIcono",1);
@@ -30,7 +31,7 @@ $folder="../";
 <html lang="en">
 <head>
 		<meta charset="utf-8" />
-		<title>.::<?php echo $idioma['AccesoSistema']?> | <?php echo $title;?> | <?php echo $idioma['TituloSistema']?>::.</title>
+		<title>.:: <?php echo $idioma['TituloSistema']?>::.</title>
 
 		<meta name="description" content="Sistema Desarrollado por Ronald Nina Layme">
 		<meta name="author" content="Sistema Desarrollado por Ronald Nina Layme">
@@ -53,7 +54,7 @@ $folder="../";
 		<link rel="stylesheet" href="<?php echo $folder?>css/core/ace-fonts.css" />
 
 		<!-- ace styles -->
-
+        <link rel="stylesheet" href="css/inicio.css">
 		<link rel="stylesheet" href="<?php echo $folder?>css/core/ace.min.css" />
 		<link rel="stylesheet" href="<?php echo $folder?>css/core/ace-rtl.min.css" />
 
@@ -78,133 +79,53 @@ $folder="../";
 			RedirigirLogin=1;
 			var AyudaTitulo="<?php echo $idioma['AyudaTitulo']?>";
 		</script>
+
 	</head>
 
-	<body class="login-layout" style="background-image:url(../imagenes/inicio2.jpg);background-size:cover;">
-		<div class="main-container">
-			<div class="main-content">
-				<div class="row">
-					<div class="col-sm-10 col-sm-offset-1">
-						<div class="login-container">
-							<div class="center">
-								<h1>
-									<i class="icon-eyes "></i>
-									<span class="red"><?php echo $title;?></span>
-
-								</h1>
-								<h4 class="blue"><?php echo $sigla;?></h4>
-							</div>
-
-							<div class="space-6"></div>
-
-							<div class="position-relative">
-								<div id="login-box" class="login-box visible widget-box no-border">
-									<div class="widget-body">
-										<div class="widget-main">
-											<h4 class="header blue lighter bigger">
-												<i class="icon-key green"></i>
-												<?php echo $idioma['AccesoSistema'] ?>
-											</h4>
-											<?php
-                    if($idiomano==1){
-                    ?>
-                    <div class="alert alert-warning">
-                    	<button type="button" class="close" data-dismiss="alert">&times;</button>
-                    	<?php echo $idioma['IdiomaNoEncontrado']?>
-                    </div>
-                    <?php
-                    }?>
-                    
-                    <?php
-                    if(isset($_GET['incompleto'])){
-                    ?>
-                    <div class="alert alert-danger">
-                    	<button type="button" class="close" data-dismiss="alert">&times;</button>
-                    	<?php echo $idioma['NoDatos']?>
-                    </div>
-                    <?php
-                    }
-                    if(isset($_GET['error'])){
-                    ?>
-                    <div class="alert alert-info">
-                    	<button type="button" class="close" data-dismiss="alert">&times;</button>
-                    	<?php echo $idioma['DatosErroneos']?>
-                    </div>
-                    <?php
-                    }
-                    ?>
-											<div class="space-6"></div>
-
-											<form action="login.php" method="post" id="login">
-                                            <input type="hidden" name="u" value="<?php echo $_GET['u'];?>" />
-												<fieldset>
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" name="usuario" id="usuario" class="form-control" placeholder="<?php echo $idioma['Usuario'] ?>" />
-															<i class="icon-user"></i>
-														</span>
-													</label>
-
-													<label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" name="pass" id="pass"  class="form-control" placeholder="<?php echo $idioma['Contraseña'] ?>" />
-															<i class="icon-lock"></i>
-														</span>
-													</label>
-
-													<div class="space"></div>
-
-													<div class="clearfix">
-
-
-														<button type="submit" class="width-35 pull-right btn btn-sm btn-primary">
-															<i class="icon-key"></i>
-															<?php echo $idioma['Ingresar'] ?>
-														</button>
-													</div>
-
-													<div class="space-4"></div>
-												</fieldset>
-											</form>
-
-
-										</div><!-- /widget-main -->
-
-										<div class="toolbar clearfix">
-											<div style="padding:10px">
-												
-                                                <small class="light-blue">Sistema Desarrollado por <br><a href="http://fb.com/ronaldnina/" class="light-blue" target="_blank" title="Cel: 73230568">Ronald Nina Layme</a></small>
-                                            </div>
-                                            <div>
-                                            	<a class="light-blue">&copy; <?php echo date("Y")?></a>
-                                            </div>
-                                            
-										</div>
-                                        
-									</div><!-- /widget-body -->
-                                    
-								</div><!-- /login-box -->
-
-								
-
-								
-							</div><!-- /position-relative -->
-                            
-						</div>
-                        
-					</div><!-- /.col -->
-				</div><!-- /.row -->
-			</div>
+	<body class="login-layout" style="background-image:url(../imagenes/fondos/<?php echo rand(7,14)?>.jpg);background-size:cover;">
+    <div id="header">
+        <h3 class="name-title"><?php echo $TituloSistema;?></h3>
+        <h1 class="name-title">
+           <?php echo $Titulo;?>
+        </h1>
+    </div>
+    <div class="login">
+        <div class="titulo">Acceso</div>
+        <hr>
+            <?php if(isset($_GET['error'])){?>
+            <div class="alert alert-info">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <small><?php echo $idioma['DatosErroneos']?></small>
+            </div>
+            <?php }?>
+            <?php if(isset($_GET['incompleto'])){?>
+            <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <small><?php echo $idioma['NoDatos']?></small>
+            </div>
+            <?php }?>
+        <form action="login.php" method="post" id="login">
+            <input type="hidden" name="u" value="<?php echo $_GET['u'];?>" />
+            <label class="block clearfix">
+                <span class="block input-icon input-icon-right">
+                <input type="text" name="usuario" id="usuario" class="form-control" placeholder="<?php echo $idioma['Usuario'] ?>" /><i class="icon-user"></i>
+                </span>
+            </label>
+    
+            <label class="block clearfix">
+                <span class="block input-icon input-icon-right">
+                <input type="password" name="pass" id="pass"  class="form-control" placeholder="<?php echo $idioma['Contraseña'] ?>" /><i class="icon-lock"></i>
+                </span>
+            </label>
+            <button type="submit" class="pull-right btn btn-sm btn-primary">
+            <i class="icon-key"></i><?php echo $idioma['Ingresar'] ?> 
+            </button>
+            <small class="pull-left">Sistema Desarrollado por <br><a href="http://fb.com/ronaldnina/" class="" target="_blank" title="Cel: 73230568">Ronald Nina Layme <br>Cel: 73230568</a></small>
             
-		</div><!-- /.main-container -->
+        </form>
+    </div>
+    <div class="creditos"><small>Todos los Derechos Reservados &copy; 2014 - <?php echo date("Y")?></small></div>
 
-		<!-- basic scripts -->
-
-		<!--[if !IE]> -->
-
-		
-
-		<!-- <![endif]-->
 		<script src="<?php echo $folder?>js/core/framework/bootstrap.min.js"></script>
 		<!--[if IE]>
 <script type="text/javascript">
