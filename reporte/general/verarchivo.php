14af895bb793569ce3e24832dcfd6464206db09e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-09 01:12:45 -0400

    Reporte General

--- reporte/general/verarchivo.php
+++ reporte/general/verarchivo.php
@@ -6,4 +6,4 @@ $Hasta=fecha2Str($_POST['Hasta'],0);
 $url="../../impresion/reporte/general.php?Desde=".$Desde."&Hasta=".$Hasta;
 ?>
 <a href="<?php echo $url?>" class="btn btn-xs btn-warning" target="_blank"><?php echo $idioma['AbrirOtraVentana']?></a><br />
-<iframe src="<?php echo $url?>" width="100%" height="700" frameborder="1"></iframe>
+<iframe src="<?php echo $url?>" width="100%" height="835" frameborder="1"></iframe>
